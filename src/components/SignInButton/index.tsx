c4f7eea8d6a891687f2c101acc5840cda5dcb46d
Alcir Junior <junior@cajucomunica.com.br>
2021-05-22 13:19:10 -0300

    feat: Add Auth Github Login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 const SignInButton = () => {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    return session ? (
+        <button className={styles.signInButton} type="button" onClick={() => signOut()}>
             <FaGithub color="#04d361" />
-            Caju
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
