c2add3d236c76f83e01172e5f630fbffd5566271
Alcir Junior <junior@cajucomunica.com.br>
2021-05-23 11:51:59 -0300

    feat: Add Posts Component

---
+++ src/pages/posts.tsx
@@ -0,0 +1,11 @@
+import styles from './posts.module.scss';
+
+const Posts = () => {
+    return (
+        <div className={styles.contentPosts}>
+            <h1>Posts</h1>
+        </div>
+    );
+};
+
+export default Posts;
