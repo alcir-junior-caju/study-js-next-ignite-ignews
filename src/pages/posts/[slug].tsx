cb0b0cdf3f9b531ae45d906806e994d42e31f6e3
Alcir Junior <junior@cajucomunica.com.br>
2021-05-23 17:51:45 -0300

    feat: Add Check Auth Post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req });
     const { slug } = params;
 
-    // if (!session)
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     const prismic = getPrismicClient(req);
     const response = await prismic.getByUID('post', String(slug), {});
